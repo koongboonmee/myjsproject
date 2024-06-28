e5f64be1eeb06615f365a3b83b9bd04fb781a10a
koongboonmee <164847643+koongboonmee@users.noreply.github.com>
2024-06-28 13:37:31 +0700

    Update math.test.js

--- math.test.js
+++ math.test.js
@@ -1,19 +1,16 @@
-const sum = require('./math');
-
-test('Adds 1 + 2 to equal 3', () => {
-    expect(sum(1,2)).toBe(3);
+const {sum, multiply, divide} = require('./math');
+test('add 1+2 to equal 3', () => {
+     expect(sum(1, 2)).toBe(3); 
 })
 
-const sum = require('./sum');
+test('multiply 2*3 to equal 6', () => {
+        expect(multiply(2, 3)).toBe(6); 
+    })
 
-test('multiples 2 * 3 to equal 6', () => {
-    expect(sum(2,3)).toBe(6);
-})
+test('divide 6/3 to equal 2', () => {
+        expect(divide(6, 3)).toBe(2); 
+    })
 
-test('divides 6 / 2 to equal 3', () => {
-    expect(sum(6,2)).toBe(3);
-})
-
-test('divides by zero throws error', () => {
-    expect(() => divide(6,0)).toThrow('Cannot divide by zero');
-});
+    test('divide 6/0 to throw error', () => {
+        expect(() => divide(6, 0)).toThrow('Cannot divide by zero'); 
+    })
