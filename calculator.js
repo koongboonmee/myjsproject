cb7f3d83d8cd267738a0f5f25660e7add71bfeee
koongboonmee <164847643+koongboonmee@users.noreply.github.com>
2024-06-28 13:35:25 +0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,9 +1,9 @@
-const { sum , multiply , devide, divide } = require('./math');
+const { sum, multiply, divide } = require('./math');
 
 function calculate(a, b, c) {
-    const additionResult = sum(a,b);
-    const multiplicationResult = multiply(additionResult, c);
-    return divide(multiplicationResult, 2)
+  const additionResult = sum(a, b);
+  const multiplicationResult = multiply(additionResult, c);
+  return divide(multiplicationResult, 2);
 }
 
 module.exports = calculate;
